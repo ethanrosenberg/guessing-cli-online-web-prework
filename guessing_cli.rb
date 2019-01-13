308cd382aad9623e0ecc28335d6cbd0eca96ee55
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-13 07:44:37 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   random = rand(1..6)
   input = gets.chomp
-  
+  binding.pry
   if input == "exit"
     puts "Goodbye!"
   elsif input == random.to_s
